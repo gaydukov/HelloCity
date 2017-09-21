87861368901089b51e5e7c80050518ccb6d58298
gaydukov <gaydukov_s@bigmir.net>
2017-09-21 11:45:46 +0300

    Update TimeOfDayTest.java

--- src/test/java/TimeOfDayTest.java
+++ src/test/java/TimeOfDayTest.java
@@ -4,7 +4,6 @@ import org.junit.Test;
 import java.util.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.TimeZone;
 
 
@@ -12,22 +11,20 @@ public class TimeOfDayTest {
     @Test
     public void timeHourCityNameTest(){
         TimeOfDay timeOfDay=new TimeOfDay();
-        //TimeZone.getTimeZone("Europe/Kiev");
         DateFormat dateFormat = new SimpleDateFormat("HH");
         dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Kiev"));
-        //Calendar calendar = Calendar.getInstance();
         Date date=new Date();
         int hour = Integer.parseInt(dateFormat.format(date.getTime()));
-        //int hour = Integer.parseInt(dateFormat.format(calendar.getTime()));
         Assert.assertEquals(hour,timeOfDay.timeHourCityName("Kiev"));
     }
-   /* @Test
+    @Test
     public void timeHourZonaTest(){
         TimeOfDay timeOfDay=new TimeOfDay();
         DateFormat dateFormat = new SimpleDateFormat("HH");
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Kiev"));
-        int hour = Integer.parseInt(dateFormat.format(calendar.getTime()));
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Kiev"));
+        Date date=new Date();
+        int hour = Integer.parseInt(dateFormat.format(date.getTime()));
         Assert.assertEquals(hour,timeOfDay.timeHourZona("Europe/Kiev"));
-    }*/
+    }
 
 }
