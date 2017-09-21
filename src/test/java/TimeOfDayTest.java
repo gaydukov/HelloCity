8d7111475f370e351e059ec700aa63ba2349c129
gaydukov <gaydukov_s@bigmir.net>
2017-09-21 11:00:06 +0300

    Update TimeOfDayTest.java

--- src/test/java/TimeOfDayTest.java
+++ src/test/java/TimeOfDayTest.java
@@ -9,15 +9,16 @@ import java.util.TimeZone;
 
 
 public class TimeOfDayTest {
-  /*  @Test
+    @Test
     public void timeHourCityNameTest(){
         TimeOfDay timeOfDay=new TimeOfDay();
+        TimeZone.getTimeZone("Europe/Kiev")
         DateFormat dateFormat = new SimpleDateFormat("HH");
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Europe/Kiev"));
+        Calendar calendar = Calendar.getInstance();
         int hour = Integer.parseInt(dateFormat.format(calendar.getTime()));
         Assert.assertEquals(hour,timeOfDay.timeHourCityName("Kiev"));
     }
-    @Test
+   /* @Test
     public void timeHourZonaTest(){
         TimeOfDay timeOfDay=new TimeOfDay();
         DateFormat dateFormat = new SimpleDateFormat("HH");
